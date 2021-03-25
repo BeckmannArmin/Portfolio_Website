ce284a2b8425e340f18e13351b4b22b3ff220c08
Armin <40383183+Preefix@users.noreply.github.com>
2021-03-25 14:16:23 +0100

    possible fix for mobile safari

--- src/components/sections/AboutMe_v2.vue
+++ src/components/sections/AboutMe_v2.vue
@@ -29,7 +29,9 @@
     <svg
       class="about-svg"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 300 350"
+      viewBox="0 0 200 200"
+      width="300"
+      height="350"
     >
       <g id="shapes-about">
         <text opacity="0" fill="#88d3d1">&#35;</text>
